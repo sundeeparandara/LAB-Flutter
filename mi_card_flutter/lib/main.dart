961e93df48272b19d350cba0f5331215aa3f888b
SLA <40652679+sarandara@users.noreply.github.com>
2021-01-04 18:55:24 +0000

    Update main.dart

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -10,7 +10,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
-        body: Container(),
+        appBar: AppBar(
+          title: Text('This is an app'),
+        ),
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.only(left: 30), //for the outside of your widget
+            padding: EdgeInsets.all(20), //for the inside of your widget
+            color: Colors.white,
+            child: Text('Hello'),
+          ),
+        ),
       ),
     );
   }
