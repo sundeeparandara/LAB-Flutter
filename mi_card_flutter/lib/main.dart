3a4e917ab2a7b7f6a9ad5d81241a5a92fbb4a720
SLA <40652679+sarandara@users.noreply.github.com>
2021-01-05 18:52:56 +0000

    Update main.dart

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -9,18 +9,81 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('This is an app'),
-        ),
+        backgroundColor: Colors.teal,
+//        appBar: AppBar(
+//          title: Text('This is an app'),
+//        ),
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.only(left: 30), //for the outside of your widget
-            padding: EdgeInsets.all(20), //for the inside of your widget
-            color: Colors.white,
-            child: Text('Hello'),
+//          child: Row(
+//            //mainAxisSize: MainAxisSize.min,
+//            //verticalDirection: VerticalDirection.up,
+//            //mainAxisAlignment: MainAxisAlignment.end,
+//            //mainAxisAlignment: MainAxisAlignment.center,
+//            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//            //crossAxisAlignment: CrossAxisAlignment.end,
+//            crossAxisAlignment: CrossAxisAlignment.stretch,
+//            children: [
+//              Container(
+//                height: 100.0,
+//                width: 100.0,
+//                margin:
+//                    EdgeInsets.only(left: 20), //for the outside of your widget
+//                padding: EdgeInsets.all(20), //for the inside of your widget
+//                color: Colors.white,
+//                child: Text('C1'),
+//              ),
+//              SizedBox(
+//                //height: 20,
+//                width: 20,
+//              ),
+//              Container(
+//                height: 100.0,
+//                width: 100.0,
+//                margin:
+//                    EdgeInsets.only(left: 0), //for the outside of your widget
+//                padding: EdgeInsets.all(20), //for the inside of your widget
+//                color: Colors.green,
+//                child: Text('C2'),
+//              ),
+//              Container(
+//                height: 100.0,
+//                width: 100.0,
+//                margin:
+//                    EdgeInsets.only(left: 0), //for the outside of your widget
+//                padding: EdgeInsets.all(0), //for the inside of your widget
+//                color: Colors.orangeAccent,
+//                child: Text('C2'),
+//              ),
+//            ],
+//          ), // STANDARD
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                color: Colors.red,
+                width: 100,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+              Container(
+                color: Colors.blue,
+                width: 100,
+              )
+            ],
           ),
         ),
       ),
