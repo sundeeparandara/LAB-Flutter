8d98c12764179db81a9270f3149b3f3a99bbad47
SLA <40652679+sarandara@users.noreply.github.com>
2021-01-19 06:18:33 +0000

    Update main.dart

--- s07_dicee_flutter/lib/main.dart
+++ s07_dicee_flutter/lib/main.dart
@@ -18,14 +18,25 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: Image(
-            image: AssetImage('images/dice1.png'),
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            //flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice1.png'),
+            ),
           ),
-        ),
-      ],
+          Expanded(
+            //flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
